ea72208f42e6293805c58290335214ee19565392
Yongjeong Oh <yongjeongoh@gmail.com>
2026-05-07 15:37:32 +0800

    Deploy updated citation counts to GitHub Pages

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -9,7 +9,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yongjeong-oh/yongjeong-oh.github.io",
+    "data-repo": "yongjeong-oh/homepage-source",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
